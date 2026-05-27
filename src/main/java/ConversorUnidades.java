2e241269c2de378ea0554eb5b3d46b4011f63a72
Gabriel Dias Gonçalves <gabrieldiasgonca@gmail.com>
2026-05-27 01:05:52 -0300

    simulando erro

--- src/main/java/ConversorUnidades.java
+++ src/main/java/ConversorUnidades.java
@@ -2,7 +2,7 @@ public class ConversorUnidades {
 
     // Método 1: Quilômetros para Milhas (1 km = 0.621371 milhas)
     public double quilometrosParaMilhas(double km) {
-        return km * 0.621371;
+         km * 0.621371;
     }
 
     // Método 2: Centímetros para Polegadas (1 cm = 0.393701 polegadas)
